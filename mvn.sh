1efc604a1a8c73aaa8a866b4f2b044cabd1e835c
Thomas Lagae <thomas.lagae@gmail.com>
2018-05-17 10:25:02 +0200

    Changed port

--- mvn.sh
+++ mvn.sh
@@ -2,6 +2,6 @@
 
 # Deprecated option -XX:MaxPermSize=256m is kept
 # just in case someone still uses java 1.7
-export MAVEN_OPTS="-Xmx1024m -Xms1024m -Xss2048k -XX:MaxPermSize=1024m"
+export MAVEN_OPTS="-Xmx1024m -Xms1024m -Xss2048k -XX:MaxPermSize=1024m -Djetty.port=8081"
 
 mvn $1 $2 $3 $4
